498790c06a4dc8808523b113e9dde6d13800ab22
helal119465 <helal877906@gmail.com>
2024-01-12 22:54:57 +0600

    addToCart

--- src/app/Slices/addToCart.jsx
+++ src/app/Slices/addToCart.jsx
@@ -3,13 +3,33 @@ import { createSlice} from "@reduxjs/toolkit";
 export const addToCart=createSlice({
     name:"addToCart",
     initialState:{
-        cartData:[]
+        cartData:JSON.parse(localStorage.getItem("cartData"))?JSON.parse(localStorage.getItem("cartData")):[],
     },
     reducers:{
         addToCartFun:(state,action)=>{
-            state.cartData=[...state.cartData,action.payload]
+            let findProduct=state.cartData.findIndex((item)=>item.id==action.payload.id)
+            console.log(findProduct)
+            if(findProduct!=-1){
+                state.cartData[findProduct].quantity++;
+                localStorage.setItem('cartData',JSON.stringify(state.cartData))
+            }else{
+                state.cartData=[...state.cartData,action.payload]
+                localStorage.setItem('cartData',JSON.stringify(state.cartData))
+            }
+        },
+        removeToCart:(state,action)=>{
+            state.cartData.splice(action.payload,1)
+            localStorage.setItem('cartData',JSON.stringify(state.cartData))
+        },
+        quantityUpdate:(state,action)=>{
+            state.cartData[action.payload].quantity+=1;
+            localStorage.setItem('cartData',JSON.stringify(state.cartData))
+        },
+        quantityMinus:(state,action)=>{
+            state.cartData[action.payload].quantity-=1
+            localStorage.setItem('cartData',JSON.stringify(state.cartData))
         }
     }
 })
-export const {addToCartFun}=addToCart.actions
+export const {addToCartFun,removeToCart,quantityUpdate,quantityMinus}=addToCart.actions
 export default addToCart.reducer;
--- src/component/Cart.jsx
+++ src/component/Cart.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Breadcrum from './Breadcrum'
 import Flex from './layout/Flex'
 import Container from './layout/Container'
@@ -7,7 +7,19 @@ import CartImg from '../assets/CartImg.png'
 import Img from './layout/Img';
 import { FiPlus,FiMinus } from "react-icons/fi";
 import Button from './layout/Button';
+import { useDispatch, useSelector } from 'react-redux';
+import { quantityMinus, quantityUpdate, removeToCart } from '../app/Slices/addToCart';
 const Cart = () => {
+  let dispatch=useDispatch()
+  const cartData=useSelector((state)=>state.addtocart.cartData)
+  let [total,setTotal]=useState(0)
+  useEffect(()=>{
+    let totalPrice=0;
+    cartData.map((product)=>{
+      totalPrice+=Math.ceil((product.price-(product.price*product.discountPercentage)/100)*product.quantity)
+    })
+    setTotal(totalPrice)
+  },[cartData])
   return (
     <section className=' my-10'>
       <Container>
@@ -17,32 +29,36 @@ const Cart = () => {
         />
         <Flex className={'mt-12 flex-col gap-y-3 text-[#262626]  font-bold'}>
           <Flex className={'bg-[#F5F5F3] py-7 w-full justify-around text-base'}>
-            <div className='w-[40%]'>Product</div>
-            <div className='w-[18%]'>Price</div>
-            <div className='w-[18%]'>Quantity</div>
-            <div className='w-[15%]'>Total</div>
+            <div className='w-[33%]'>Product</div>
+            <div className='w-[15%]'>Price</div>
+            <div className='w-[14%]'>Discount</div>
+            <div className='w-[15%]'>Quantity</div>
+            <div className='w-[14%] text-center'>Total</div>
           </Flex>
-          <Flex className={'flex-col gap-y-2'}>
-            <Flex className={'w-full justify-around py-2 border border-slate-300 items-center'}>
-                <div className='w-[40%]'>
+          {cartData.map((product,index)=>(
+            <Flex className={'flex-col gap-y-2'}>
+            <Flex className={'w-full justify-around py-3 border border-slate-300 items-center'}>
+                <div className='w-[33%]'>
                     <Flex className={'gap-x-4 items-center'}>
-                        <ImCross/>
-                        <Img src={CartImg} className={'w-[100px] h-[100px]'}/>
-                        <h1>Product Name</h1>
+                        <ImCross className='cursor-pointer'onClick={()=>dispatch(removeToCart(index))}/>
+                        <Img src={product.thumbnail} imgClassName={'w-[80px] h-[80px]'}/>
+                        <h1>{product.title}</h1>
                     </Flex>
                 </div>
-                <div className='w-[18%]'>$44.00</div>
-                <div className='w-[18%]'>
+                <div className='w-[15%]'>${product.price}</div>
+                <div className='w-[14%]'>{product.discountPercentage}%</div>
+                <div className='w-[15%]'>
                     <Flex className={'items-center gap-x-3'}>
-                        <button><FiMinus /></button>
-                        <input type='number' value={"1"} className='w-14 h-9 border border-gray-300 text-center'/>
-                        <button><FiPlus /></button>
+                        <button onClick={()=>dispatch(quantityMinus(index))}
+                         disabled={product.quantity==1 && true} ><FiMinus /></button>
+                        <input type='number' value={product.quantity} className='w-14 h-9 border border-gray-300 text-center'/>
+                        <button onClick={()=>dispatch(quantityUpdate(index))}><FiPlus /></button>
                     </Flex>
                 </div>
-                <div className='w-[15%]'>$44.00</div>
+                <div className='w-[14%] text-center'>${Math.ceil((product.price-(product.price*product.discountPercentage)/100)*product.quantity)}</div>
             </Flex>
             <Flex className={'w-full px-4 py-3 border border-slate-300 items-center font-normal text-sm'}>
-                <select className='w-[255px] outline-none border border-[#EAEAEA] text-[#767676] text-base  py-2 px-4'> 
+                <select className='w-[255px] outline-none border border-[#EAEAEA] text-[#262626] text-base py-2 px-4'> 
                     <option className='py-1' selected>Size</option>
                     <option>M</option>
                     <option>L</option>
@@ -52,15 +68,17 @@ const Cart = () => {
                 <input type="text" className='w-52 ml-2 outline-none px-2 py-[3px] border border-sky-200' placeholder='apply cupon number' />
             </Flex>
           </Flex>
+          ))}
+          
         </Flex>
         <div>
           <h1 className='text-right text-xl font-bold mt-10'>Cart Totals</h1>
         </div>
-        <Flex className={'w-[644px] ml-auto items-center text-[#262626] text-base font-bold flex-wrap mt-6'}>
+        <Flex className={'w-[644px] ml-auto items-center text-[#767676] text-base font-bold flex-wrap mt-6'}>
           <h1 className='w-1/2 border py-3 pl-4'>Subtotal</h1>
-          <h1 className='w-1/2 border border-l-0 py py-3 pl-4 text-[#767676]'>44.00$</h1>
+          <h1 className='w-1/2 border border-l-0 py py-3 pl-4 text-[#262626]'>{total}$</h1>
           <h1 className='w-1/2 border py-3 pl-4'>Total</h1>
-          <h1 className='w-1/2 border border-l-0 py py-3 pl-4 text-[#767676]'>44.00$</h1>
+          <h1 className='w-1/2 border border-l-0 py py-3 pl-4 text-[#262626]'>{total}$</h1>
         </Flex>
         <div className=' text-right mt-8'>
           <Button title={'Proceed to Checkout'}
