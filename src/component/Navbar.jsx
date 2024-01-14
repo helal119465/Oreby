ac99686e316a8589489eb2ab480dca1c72ad5dd7
helal119465 <helal877906@gmail.com>
2024-01-15 02:16:10 +0600

    Navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,43 +1,85 @@
-import Container from './layout/Container'
-import React, { useEffect, useState } from 'react'
-import Flex from './layout/Flex'
-import Img from './layout/Img'
-import Logo from '../assets/Logo.png'
-import List from './layout/List'
-import ListItem from './layout/ListItem'
+import Container from "./layout/Container";
+import React, { useEffect, useState } from "react";
+import Flex from "./layout/Flex";
+import Img from "./layout/Img";
+import Logo from "../assets/Logo.png";
+import List from "./layout/List";
+import ListItem from "./layout/ListItem";
 import { FaBars } from "react-icons/fa";
 
 const Navbar = () => {
-  let [manuShow,setManuShow]=useState(true)
-  useEffect(()=>{
-    const resize=()=>{
-      if(window.innerWidth<641){
-        setManuShow(false)
-      }else{
-        setManuShow(true)
+  let [manuShow, setManuShow] = useState(true);
+  useEffect(() => {
+    const resize = () => {
+      if (window.innerWidth < 641) {
+        setManuShow(false);
+      } else {
+        setManuShow(true);
       }
-    }
-    window.addEventListener("resize",resize);
+    };
+    window.addEventListener("resize", resize);
     resize();
-  },[])
+  }, []);
+  console.log(window.innerWidth);
   return (
-    <nav className='font-dm border-y-2 border-[#F5F5F3]'>
-        <Container>
-            <Flex className={"sm:h-16 h-4 items-center mx-2 relative z-10"}>
-                <Img src={Logo} className={"sm:w-[10%] w-[20%] "} imgClassName={''}/>
-                {manuShow && (<List className={"sm:w-[90%] w-full flex justify-center gap-x-10 sm:text-[#767676] text-white text-lg flex-col sm:flex-row sm:relative absolute top-16 sm:top-0 bg-cyan-950 sm:bg-transparent rounded-md sm:rounded-none text-center "}>
-                  <ListItem item={"Home"} link={'/'} className={"sm:hover:text-[#262626] transition-colors sm:py-0 py-4 block sm:border-none border-b border-[#767676]"}/>
-                  <ListItem item={"Shop"} link={'/shop'} className={"sm:hover:text-[#262626] transition-colors sm:py-0 py-4 block sm:border-none border-b border-[#767676]"}/>
-                  <ListItem item={"About"} link={'/about'} className={"sm:hover:text-[#262626] transition-colors sm:py-0 py-4 block sm:border-none border-b border-[#767676]"}/>
-                  <ListItem item={"Contacts"} link={'/contacts'} className={"sm:hover:text-[#262626] transition-colors sm:py-0 py-4 block sm:border-none border-b border-[#767676]"}/>
-                  <ListItem item={"Journal"} link={'/journal'} className={"sm:hover:text-[#262626] transition-colors sm:py-0 py-4 block sm:border-none border-b border-[#767676]"}/>
-                </List>)}
-                
-                <FaBars className='sm:hidden cursor-pointer absolute right-0' onClick={()=>setManuShow(!manuShow)}/>
-            </Flex>
-        </Container>
-    </nav>
-  )
-}
+    <nav className="font-dm border-y-2 border-[#F5F5F3] px-2 py-4">
+      <Container className={"sm:flex justify-between "}>
+        <Flex className={"items-center self-auto relative"}>
+          <Img src={Logo} className={" w-full"} />
+          <FaBars
+            className="sm:hidden cursor-pointer absolute right-0"
+            onClick={() => setManuShow(!manuShow)}
+          />
+        </Flex>
 
-export default Navbar
+        <Flex className={"items-center"}>
+          {manuShow && (
+            <List
+              className={
+                " sm:w-[90%] w-full flex justify-center gap-x-10 mt-4 sm:mt-0 sm:text-[#767676] text-cyan-300 text-lg flex-col sm:flex-row bg-slate-800 sm:bg-transparent text-center font-semibold"
+              }
+            >
+              <ListItem
+                item={"Home"}
+                link={"/"}
+                linkClassName={
+                  "sm:hover:text-[#262626] hover:text-cyan-600 transition-all sm:py-0 py-4 block sm:border-none border-b border-[#767676]"
+                }
+              />
+              <ListItem
+                item={"Shop"}
+                link={"/shop"}
+                linkClassName={
+                  "sm:hover:text-[#262626] hover:text-cyan-600 transition-all sm:py-0 py-4 block sm:border-none border-b border-[#767676]"
+                }
+              />
+              <ListItem
+                item={"About"}
+                link={"/about"}
+                linkClassName={
+                  "sm:hover:text-[#262626] hover:text-cyan-600 transition-all sm:py-0 py-4 block sm:border-none border-b border-[#767676]"
+                }
+              />
+              <ListItem
+                item={"Contacts"}
+                link={"/contacts"}
+                linkClassName={
+                  "sm:hover:text-[#262626] hover:text-cyan-600 transition-all sm:py-0 py-4 block sm:border-none border-b border-[#767676]"
+                }
+              />
+              <ListItem
+                item={"Journal"}
+                link={"/journal"}
+                linkClassName={
+                  "sm:hover:text-[#262626] hover:text-cyan-600 transition-all sm:py-0 py-4 block sm:border-none border-b border-[#767676]"
+                }
+              />
+            </List>
+          )}
+        </Flex>
+      </Container>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/component/layout/DropDown.jsx
+++ src/component/layout/DropDown.jsx
@@ -1,37 +1,72 @@
-import React, { useState,useRef,useEffect } from 'react'
+import React, { useState, useRef, useEffect } from "react";
 import { HiBars3 } from "react-icons/hi2";
-import DropDownItem from './DropDownItem';
-import List from './List';
-import ListItem from './ListItem';
+import DropDownItem from "./DropDownItem";
+import List from "./List";
+import ListItem from "./ListItem";
 
 const DropDown = () => {
-  let [show,setShow]=useState(false)
-  let category=useRef()
-  useEffect(()=>{
-    document.body.addEventListener("click",((e)=>{
-      if(category.current.contains(e.target)){
-            setShow(!show)
-        }else{
-          setShow(false)
-        }
-    }))
-  })
+  let [show, setShow] = useState(false);
+  let category = useRef();
+  useEffect(() => {
+    document.body.addEventListener("click", (e) => {
+      if (category.current.contains(e.target)) {
+        setShow(!show);
+      } else {
+        setShow(false);
+      }
+    });
+  });
   return (
-    <div className='sm:w-[10%] md:w-[15%] w-[8%] text-[#262626] font-dm text-sm flex items-center relative z-10' ref={category}>
-      <HiBars3 className='mr-3 text-xl cursor-pointer'/> 
-      <span className='hidden lg:inline-block'>Shop by Category</span> 
-      {show && <DropDownItem className={'absolute top-10'}>
-        <List className={'w-[263px] bg-[#262626] text-[#FFFFFFB2]'}>
-          <ListItem item={"Accesories"} className={'block py-4 pl-5 hover:text-[#FFFFFF] hover:pl-7 transition-all border-b border-[#2D2D2D]'}/>
-          <ListItem item={"Furniture"} className={'block py-4 pl-5 hover:text-[#FFFFFF] hover:pl-7 transition-all border-b border-[#2D2D2D]'}/>
-          <ListItem item={"Electronics"} className={'block py-4 pl-5 hover:text-[#FFFFFF] hover:pl-7 transition-all border-b border-[#2D2D2D]'}/>
-          <ListItem item={"Clothes"} className={'block py-4 pl-5 hover:text-[#FFFFFF] hover:pl-7 transition-all border-b border-[#2D2D2D]'}/>
-          <ListItem item={"Bags"} className={'block py-4 pl-5 hover:text-[#FFFFFF] hover:pl-7 transition-all border-b border-[#2D2D2D]'}/>
-          <ListItem item={"Home appliances"} className={'block py-4 pl-5 hover:text-[#FFFFFF] hover:pl-7 transition-all '}/>
-        </List>
-      </DropDownItem>}
-      </div>
-  )
-}
+    <div
+      className="sm:w-[10%] md:w-[15%] w-[8%] text-[#262626] font-dm text-sm flex items-center relative z-10"
+      ref={category}
+    >
+      <HiBars3 className="mr-3 text-xl cursor-pointer" />
+      <span className="hidden lg:inline-block">Shop by Category</span>
+      {show && (
+        <DropDownItem className={"absolute top-10"}>
+          <List className={"w-[263px] bg-[#262626] text-[#FFFFFFB2]"}>
+            <ListItem
+              item={"Accesories"}
+              className={
+                "block py-4 pl-5 hover:text-[#FFFFFF] hover:pl-7 transition-all border-b border-[#2D2D2D]"
+              }
+            />
+            <ListItem
+              item={"Furniture"}
+              className={
+                "block py-4 pl-5 hover:text-[#FFFFFF] hover:pl-7 transition-all border-b border-[#2D2D2D]"
+              }
+            />
+            <ListItem
+              item={"Electronics"}
+              className={
+                "block py-4 pl-5 hover:text-[#FFFFFF] hover:pl-7 transition-all border-b border-[#2D2D2D]"
+              }
+            />
+            <ListItem
+              item={"Clothes"}
+              className={
+                "block py-4 pl-5 hover:text-[#FFFFFF] hover:pl-7 transition-all border-b border-[#2D2D2D]"
+              }
+            />
+            <ListItem
+              item={"Bags"}
+              className={
+                "block py-4 pl-5 hover:text-[#FFFFFF] hover:pl-7 transition-all border-b border-[#2D2D2D]"
+              }
+            />
+            <ListItem
+              item={"Home appliances"}
+              className={
+                "block py-4 pl-5 hover:text-[#FFFFFF] hover:pl-7 transition-all "
+              }
+            />
+          </List>
+        </DropDownItem>
+      )}
+    </div>
+  );
+};
 
-export default DropDown
+export default DropDown;
