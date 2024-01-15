a72d19ec9e27476aceef6a6c96112e69c601a569
helal119465 <helal877906@gmail.com>
2024-01-16 05:16:43 +0600

    Product

--- src/component/layout/Pagination.jsx
+++ src/component/layout/Pagination.jsx
@@ -1,28 +1,27 @@
-import React, { useEffect, useState } from 'react';
-import ReactPaginate from 'react-paginate';
-import Products from './Products';
-import Flex from './Flex';
+import React, { useState } from "react";
+import ReactPaginate from "react-paginate";
+import Products from "./Products";
+import Flex from "./Flex";
 
-const Pagination = ({itemsPerPage,data}) => {
-// Example items, to simulate fetching from another resources.
-const items = data;
-function Items({ currentItems }) {
-    console.log(data.id)
+const Pagination = ({ itemsPerPage, data }) => {
+  // Example items, to simulate fetching from another resources.
+  const items = data;
+  function Items({ currentItems }) {
     return (
       <>
         {currentItems &&
           currentItems.map((products) => (
             <Products
-                key={products.id}
-                link={`/shop/${products.id}`}
-                className={'w-full sm:w-[47%] lg:w-[31.5%]'}
-                label={products.brand}
-                labelShow={true}
-                ProductImg={products.thumbnail}
-                ProductTitle={products.category}
-                Price={products.price}
-                // link={}
-              />
+              key={products.id}
+              link={`/shop/${products.id}`}
+              className={"w-full sm:w-[47%] lg:w-[30.5%]"}
+              label={products.brand}
+              labelShow={true}
+              ProductImg={products.thumbnail}
+              ProductTitle={products.category}
+              Price={products.price}
+              // link={}
+            />
           ))}
       </>
     );
@@ -30,35 +29,39 @@ function Items({ currentItems }) {
 
   // Here we use item offsets; we could also use page offsets
   // following the API or data you're working with.
-    const [itemOffset, setItemOffset] = useState(0);
-  
-    // Simulate fetching items from another resources.
-    // (This could be items from props; or items loaded in a local state
-    // from an API endpoint with useEffect and useState)
-    const endOffset = itemOffset + itemsPerPage;
-    console.log(`Loading items from ${itemOffset} to ${endOffset}`);
-    const currentItems = items.slice(itemOffset, endOffset);
-    const pageCount = Math.ceil(items.length / itemsPerPage);
-  
-    // Invoke when user click to request another page.
-    const handlePageClick = (event) => {
-      const newOffset = (event.selected * itemsPerPage) % items.length;
-      console.log(
-        `User requested page number ${event.selected}, which is offset ${newOffset}`
-      );
-      setItemOffset(newOffset);
-    };
-  
-    return (
-      <>
-        <Flex className={'flex-wrap gap-y-10 gap-x-6 justify-center mb-12'}>
-            <Items currentItems={currentItems} />
-        </Flex>
-        <Flex className={'justify-between items-center'}>
+  const [itemOffset, setItemOffset] = useState(0);
+
+  // Simulate fetching items from another resources.
+  // (This could be items from props; or items loaded in a local state
+  // from an API endpoint with useEffect and useState)
+  const endOffset = itemOffset + itemsPerPage;
+  console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+  const currentItems = items.slice(itemOffset, endOffset);
+  const pageCount = Math.ceil(items.length / itemsPerPage);
+
+  // Invoke when user click to request another page.
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % items.length;
+    console.log(
+      `User requested page number ${event.selected}, which is offset ${newOffset}`
+    );
+    setItemOffset(newOffset);
+  };
+
+  return (
+    <>
+      <Flex className={"flex-wrap gap-y-10 gap-x-6 justify-center mb-12"}>
+        <Items currentItems={currentItems} />
+      </Flex>
+      <Flex
+        className={
+          "xl:justify-between xl:items-center flex-col xl:flex-row gap-y-10 xl:gap-y-0"
+        }
+      >
         <ReactPaginate
-          className='flex gap-x-5'
+          className="flex gap-x-5"
           pageLinkClassName="border-[1px] border-[#F0F0F0] hover:border-gray-800 font-semibold py-2 px-4 duration-500"
-          activeLinkClassName='bg-black text-white'
+          activeLinkClassName="bg-black text-white"
           breakLabel="..."
           nextLabel=""
           onPageChange={handlePageClick}
@@ -66,12 +69,14 @@ function Items({ currentItems }) {
           pageCount={pageCount}
           previousLabel=""
           renderOnZeroPageCount={null}
-          />
-          <p className='text-[#767676] text-sm font-mono'>Products from {itemOffset==0?1:itemOffset} to {endOffset>=100?100:endOffset} of {items.length} </p>
-        </Flex>
-
-      </>
-    );
-}
+        />
+        <p className="text-[#767676] text-sm font-mono xl:ml-0 ml-5">
+          Products from {itemOffset == 0 ? 1 : itemOffset} to{" "}
+          {endOffset >= 100 ? 100 : endOffset} of {items.length}{" "}
+        </p>
+      </Flex>
+    </>
+  );
+};
 
 export default Pagination;
--- src/component/layout/Products.jsx
+++ src/component/layout/Products.jsx
@@ -1,32 +1,61 @@
-import React from 'react'
-import Img from './Img'
-import Flex from './Flex'
-import { FaHeart,FaShoppingCart } from "react-icons/fa";
+import React from "react";
+import Img from "./Img";
+import Flex from "./Flex";
+import { FaHeart, FaShoppingCart } from "react-icons/fa";
 import { FaCodeCompare } from "react-icons/fa6";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-const Products = ({className,labelShow,label,ProductImg,ProductTitle,Price,color,link}) => {
-    let setLabelShow=labelShow
+const Products = ({
+  className,
+  labelShow,
+  label,
+  ProductImg,
+  ProductTitle,
+  Price,
+  color,
+  link,
+}) => {
+  let setLabelShow = labelShow;
   return (
-    <Link to={link} className={` font-d ${className} border-[1px]`}>
-        <div className='relative group overflow-hidden '>
-          {setLabelShow && <div className='absolute top-2 left-3 bg-[#262626]  text-[#FFFFFF] text-sm font-bold py-2 px-4 capitalize '>{label}</div>}
-            <Img src={ProductImg} imgClassName={"w-full lg:w-[382px] lg:h-[250px]"}/>
-            <div className=' bg-white w-full -bottom-80 absolute   capitalize text-right py-4 pr-[30px] text-[#767676] transition-all group-hover:bottom-0 duration-700 border-b-[1px] border-x-gray-400'>
-              <h6 className='hover:text-[#262626] cursor-pointer'>Add to Wish List <FaHeart className='inline-block text-[#262626] ml-3'/></h6>
-              <h6 className='hover:text-[#262626] cursor-pointer py-2'>Compare <FaCodeCompare className='inline-block text-[#262626] ml-3'/></h6>
-              <h6 className='hover:text-[#262626] cursor-pointer'>Add to Cart <FaShoppingCart className='inline-block text-[#262626] ml-3'/></h6>
-            </div>
+    <Link to={link} className={` font-dm ${className} border-[1px]`}>
+      <div className="relative group overflow-hidden ">
+        {setLabelShow && (
+          <div className="absolute top-2 left-3 bg-[#262626] text-[#FFFFFF] text-sm font-bold py-2 px-4 capitalize ">
+            {label}
+          </div>
+        )}
+        <Img
+          src={ProductImg}
+          imgClassName={
+            "xl:w-[382px] xl:h-[220px] md:w-[350px] md:h-[180px] w-full h-80 "
+          }
+        />
+        <div className=" bg-white w-full -bottom-80 absolute   capitalize text-right py-4 pr-[30px] text-[#767676] transition-all group-hover:bottom-0 duration-700 border-b-[1px] border-x-gray-400">
+          <h6 className="hover:text-[#262626] cursor-pointer">
+            Add to Wish List{" "}
+            <FaHeart className="inline-block text-[#262626] ml-3" />
+          </h6>
+          <h6 className="hover:text-[#262626] cursor-pointer py-2">
+            Compare{" "}
+            <FaCodeCompare className="inline-block text-[#262626] ml-3" />
+          </h6>
+          <h6 className="hover:text-[#262626] cursor-pointer">
+            Add to Cart{" "}
+            <FaShoppingCart className="inline-block text-[#262626] ml-3" />
+          </h6>
         </div>
-        <Flex className={'flex-col mt-6 mb-1 mx-1 '}>
-          <Flex className={'justify-between mb-3 items-center '}>
-            <h1 className='capitalize text-[#262626] text-base  font-bold'>{ProductTitle}</h1>
-            <h2 className='text-[#767676] text-base'>${Price}</h2>
-          </Flex>
-          <h2 className='text-[#767676] text-base'>{color}</h2>
-       </Flex>   
+      </div>
+      <Flex className={"flex-col mt-6 mb-1 mx-1 "}>
+        <Flex className={"justify-between mb-3 items-center "}>
+          <h1 className="capitalize text-[#262626] text-base  font-bold">
+            {ProductTitle}
+          </h1>
+          <h2 className="text-[#767676] text-base">${Price}</h2>
+        </Flex>
+        <h2 className="text-[#767676] text-base">{color}</h2>
+      </Flex>
     </Link>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
